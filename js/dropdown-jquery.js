f0a3d1f93aabef634d9b4aeed260ac2529d2927a
Yael Brown <yaelrbrown@gmail.com>
2019-06-29 00:04:04 -0400

    Update project
    Added some more required fields. Working on Materialize dropdown menu.

---
+++ js/dropdown-jquery.js
@@ -0,0 +1,8 @@
+// Dropdown jQuery
+
+var dropdowns = document.querySelectorAll('.dropdown-trigger')
+for (var i = 0; i < dropdowns.length; i++){
+    M.Dropdown.init(dropdowns[i]);
+}
+
+console.log("dropdown-jquery.js");
